3867b7143e06287a1afc13be47a8c3411df229b3
Thiago Vaz <thiago@martinsvaz.com>
2022-11-08 10:41:08 -0300

    Fixed unsafe cast.

--- tool/genAst.js
+++ tool/genAst.js
@@ -50,7 +50,7 @@ function bufferedWriteToFile(filepath){
 	}
     }
     function writeLn(line){
-	if(line) buf += line;
+	if(line) buf += line; //overloading 😄😄
 	buf += "\n";
     }
     appendToExitFns(flush);
@@ -68,37 +68,38 @@ function bufferedWriteToFile(filepath){
     let [writelnStmt] = bufferedWriteToFile(path.resolve(outdir, "Stmt.java"));
 
     let exprClasses = [
-	{name: "Assign", body: "Token name, Expr value"},
-	{name: "Binary", body: "Expr left, Token operator, Expr right"},
-	{name: "Call", body: "Expr callee, Token paren, List<Expr> arguments"},
-	{name: "Get", body: "Expr object, Token name"},
-	{name: "Grouping", body: "Expr expression"},
-	{name: "Literal", body: "Object value"},
-	{name: "Set", body: "Expr object, Token name, Expr value"},
-	{name: "Super", body: "Token keyword, Token method"},
-	{name: "This", body: "Token keyword"},
-	{name: "Logical", body: "Expr left, Token operator, Expr right"},
-	{name: "Unary", body: "Token operator, Expr right"},
-	{name: "Variable", body: "Token name"},
+	{name: "Assign",	body: "Token name, Expr value"},
+	{name: "Binary",	body: "Expr left, Token operator, Expr right"},
+	{name: "Call",		body: "Expr callee, Token paren, List<Expr> arguments"},
+	{name: "Get",		body: "Expr object, Token name"},
+	{name: "Grouping",	body: "Expr expression"},
+	{name: "Literal",	body: "Object value"},
+	{name: "Set",		body: "Expr object, Token name, Expr value"},
+	{name: "Super",		body: "Token keyword, Token method"},
+	{name: "This",		body: "Token keyword"},
+	{name: "Logical",	body: "Expr left, Token operator, Expr right"},
+	{name: "Unary",		body: "Token operator, Expr right"},
+	{name: "Variable",	body: "Token name"},
     ];
 
     let stmtClasses = [
-	{name: "Block", body: "List<Stmt> statements"},
-	{name: "Class", body: "Token name, Expr.Variable superClass, " +
+	{name: "Block",		body: "List<Stmt> statements"},
+	{name: "Class",		body: "Token name, Expr.Variable superClass, " +
 	 "List<Stmt.Function> methods"},
-	{name: "Expression", body: "Expr expression"},
-	{name: "Function", body: "Token name, List<Token> params, List<Stmt> body"},
-	{name: "If", body: "Expr condition, Stmt thenBranch, Stmt elseBranch"},
-	{name: "Print", body: "Expr expression"},
-	{name: "Return", body: "Token keyword, Expr value"},
-	{name: "Var", body: "Token name, Expr initializer"},
-	{name: "While", body: "Expr condition, Stmt body"},
+	{name: "Expression",	body: "Expr expression"},
+	{name: "Function",	body: "Token name, List<Token> params, List<Stmt> body"},
+	{name: "If",		body: "Expr condition, Stmt thenBranch, Stmt elseBranch"},
+	{name: "Print",		body: "Expr expression"},
+	{name: "Return",	body: "Token keyword, Expr value"},
+	{name: "Var",		body: "Token name, Expr initializer"},
+	{name: "While",		body: "Expr condition, Stmt body"},
     ];
 
     genSource(exprClasses, "Expr", writelnExpr);
     genSource(stmtClasses, "Stmt", writelnStmt);
 
     function genSource(classes, prefix, writeln){
+
 	writeln("package com.thmtvz.s2lox;");
 	writeln();
 	writeln("import java.util.List;");
@@ -106,6 +107,7 @@ function bufferedWriteToFile(filepath){
 	writeln();
 	writeln("abstract class " + prefix + " {");
 
+	//visitor interface
 	writeln("  interface Visitor<T> {");
 	
 	for(let c of classes){
@@ -114,20 +116,23 @@ function bufferedWriteToFile(filepath){
 	
 	writeln("  }");
 	writeln();
+	//end visitor
 
-	writeln("  abstract public String toString();");
-	writeln();
+	//subclasses
 	for(let c of classes){
 	    let fields = c.body.split(", ");
 	    
 	    writeln("  static class " + c.name + " extends " + prefix +" {");
-
+	    
+	    //acceptance
 	    writeln("    @Override");
 	    writeln("    <T> T accept(Visitor<T> visitor){");
 	    writeln("      return visitor.visit" + c.name + prefix + "(this);");
 	    writeln("    }");
 	    writeln();
+	    //end acceptance
 
+	    //toString hackery
 	    writeln("    @Override");
 	    writeln("    public String toString(){");
 	    
@@ -136,6 +141,11 @@ function bufferedWriteToFile(filepath){
 	    writeln();
 	    for(const field of fields){
 		const [fieldType, fieldName] = field.split(" ");
+		const rgx = /List/;
+		if(rgx.test(fieldType)){
+		    writeln("      props.add(" + prefix + ".listToString(this." +fieldName + "));");
+		    continue;
+		}
 		writeln("      props.add(this." + fieldName + ");");
 	    }
 	    
@@ -143,35 +153,41 @@ function bufferedWriteToFile(filepath){
 	    
 	    writeln("      for(Object o : props){");
 	    writeln("          if(o == null) continue;");
-	    writeln("          if(o instanceof List){");
-	    writeln("            builder.append(" + prefix + ".listToString((List)o));");
-	    writeln("            continue;");
-	    writeln("          }");
 	    writeln("          builder.append(o.toString());");
 	    writeln("      }");
 
 	    writeln("      return builder.toString();");
 	    
 	    writeln("    }");
+	    //end toString hack 
 	    writeln();
 	    
+	    //constructor
 	    writeln("    " + c.name + "(" + c.body + "){");
 	    for(let field of fields){
 		let fieldName = field.split(" ")[1];
 		writeln("      " + "this." + fieldName + " = "+ fieldName +";");
 	    }
 	    writeln("    }");
+	    //end constructor
 	    writeln();
 	    
+	    //data fields  
 	    for(let field of fields){
 		writeln("    " + "final " + field + ";");
 	    }
 	    writeln("  }");
 	    writeln();
+	    //end data fields
 	}
 
 	writeln("  abstract <T> T accept(Visitor<T> visitor);");
+	writeln();
 
+	writeln("  abstract public String toString();");
+	writeln();
+
+	//more hackery
 	writeln("  public static <T> String listToString(List<T> list){ ");
 	writeln("      StringBuilder builder = new StringBuilder(); ");
 	writeln("      for(T element : list){ ");
@@ -179,7 +195,7 @@ function bufferedWriteToFile(filepath){
 	writeln("      } ");
 	writeln("      return builder.toString(); ");
 	writeln("  } ");
-
+	//hackery end
 
 	writeln("}");
     }
@@ -187,4 +203,3 @@ function bufferedWriteToFile(filepath){
     return;
 
 })(process.argv);
-
